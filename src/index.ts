3a611b0e8e34d6a62e5c7ae87e0f4a54520e94df
Felds Liscia <dev@felds.com.br>
2021-10-01 22:22:23 -0300

    add voice diag

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import path from "path";
 import { TOKEN } from "./config";
 import { client } from "./discord";
 import { importDir } from "./util/fs";
+import { generateDependencyReport } from "@discordjs/voice";
 
+// make nodemon more usable
 console.clear();
+
+// make sure voice has everything needed to work properly
+console.log(generateDependencyReport());
 
 async function main() {
   // import all commands from the commands folder
