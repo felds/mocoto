251b9e761c527bbe3d6adcdb78d001f655a59d9e
Felds Liscia <dev@felds.com.br>
2021-10-02 01:20:11 -0300

    add leave command

---
+++ src/commands/leave.ts
@@ -0,0 +1,29 @@
+import { getVoiceConnection, VoiceConnectionStatus } from "@discordjs/voice";
+import { ApplicationCommandData } from "discord.js";
+import { addCommandHandler, registerCommand } from "../util/discord";
+
+const command: ApplicationCommandData = {
+  name: "leave",
+  description: "Leaves the voice channel.",
+  type: "CHAT_INPUT",
+};
+
+registerCommand(command);
+
+addCommandHandler(command, (interaction) => {
+  if (!interaction.inGuild()) return;
+
+  const connection = getVoiceConnection(interaction.guildId);
+
+  const isConnected =
+    connection &&
+    connection.state.status !== VoiceConnectionStatus.Disconnected;
+
+  if (!isConnected) {
+    interaction.reply({ content: "I'm not even connected…" });
+    return;
+  }
+
+  interaction.reply({ content: "See you later, crocodile! 👋" });
+  connection.disconnect();
+});
