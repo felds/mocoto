42a8c7d9b0070107dba0cd58a5d8f88e714f6443
Felds Liscia <dev@felds.com.br>
2021-10-01 21:00:00 -0300

    add sample commands

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const TOKEN = process.env.TOKEN!;
+export const GUILD_ID = process.env.GUILD_ID!;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,67 @@
-import dotenv from "dotenv";
-dotenv.config();
+import { ApplicationCommandData, Client, Intents } from "discord.js";
+import { GUILD_ID, TOKEN } from "./config";
 
-console.log(process.env);
+console.clear();
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+});
+
+client.login(TOKEN);
+
+client.on("ready", (e) => {
+  console.log("Deixa os garoto brincar");
+  const acm = client.application?.commands;
+  if (acm) {
+    const pingCommand: ApplicationCommandData = {
+      type: "CHAT_INPUT",
+      name: "ping",
+      description: "Replies with pong.",
+    };
+    acm.create(pingCommand, GUILD_ID);
+
+    const serverCommand: ApplicationCommandData = {
+      type: "CHAT_INPUT",
+      name: "server",
+      description: "Replies with server info.",
+    };
+    acm.create(serverCommand, GUILD_ID);
+
+    const userCommand: ApplicationCommandData = {
+      type: "CHAT_INPUT",
+      name: "user",
+      description: "Replies with user info.",
+    };
+    acm.create(userCommand, GUILD_ID);
+  }
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  switch (commandName) {
+    case "ping":
+      await interaction.reply({ content: "Pong", ephemeral: true });
+      break;
+    case "server":
+      await interaction.reply({
+        content: [
+          `Server name: ${interaction.guild?.name}`,
+          `Total members: ${interaction.guild?.memberCount}`,
+        ].join("\n"),
+        ephemeral: true,
+      });
+      break;
+    case "user":
+      await interaction.reply({
+        content: [
+          `Your tag: ${interaction.user.tag}`,
+          `Your id: ${interaction.user.id}`,
+        ].join("\n"),
+        ephemeral: true,
+      });
+      break;
+  }
+});
