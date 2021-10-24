85f9836300050ea3efea61fcc46a49ad38e7b349
Felds Liscia <dev@felds.com.br>
2021-10-23 23:01:28 -0300

    constrain channel type

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -1,9 +1,5 @@
-import {
-  ApplicationCommandData,
-  GuildChannel,
-  GuildMember,
-  MessageEmbed,
-} from "discord.js";
+import { ApplicationCommandData, GuildChannel, GuildMember } from "discord.js";
+import { ChannelTypes } from "discord.js/typings/enums";
 import {
   addCommandHandler,
   join,
@@ -20,6 +16,7 @@ const command: ApplicationCommandData = {
       name: "channel",
       type: "CHANNEL",
       description: "The voice channel to join.",
+      channelTypes: [ChannelTypes.GUILD_VOICE, ChannelTypes.GUILD_STAGE_VOICE],
     },
   ],
 };
