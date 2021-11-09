a2ec0567238d12415fb61b523ac2393d460f159d
Felds Liscia <dev@felds.com.br>
2021-11-08 23:05:06 -0300

    refactor function

--- src/util/string.ts
+++ src/util/string.ts
@@ -2,10 +2,11 @@ export function msToDuration(ms: number | null): string {
   if (!ms) {
     return "♾️ live";
   }
-  const date = new Date(ms);
-  const HH = `${date.getUTCHours()}`.padStart(2, "0");
-  const MM = `${date.getUTCMinutes()}`.padStart(2, "0");
-  const SS = `${date.getUTCSeconds()}`.padStart(2, "0");
 
-  return `${HH}:${MM}:${SS}`;
+  const ss = ms / 1000;
+  const s = String(ss % 60 >> 0).padStart(2, "0");
+  const m = String(((ss % 3600) / 60) >> 0).padStart(2, "0");
+  const h = String((ss / 3600) >> 0).padStart(2, "0");
+
+  return `${h}:${m}:${s}`;
 }
