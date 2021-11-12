2263c27e8fc186597f8b9e87696e194965c59c13
Manoel S. Motoso <manoel.motoso@vzlab.com.br>
2021-11-12 15:06:10 -0300

    (wip) add seek comand

---
+++ src/commands/seek.ts
@@ -0,0 +1,51 @@
+import { getVoiceConnection } from "@discordjs/voice";
+import { ApplicationCommandData, GuildMember } from "discord.js";
+import { getQueue } from "../player/queue";
+import { addCommandHandler, join, registerCommand } from "../util/discord";
+import { msToDuration } from "../util/string";
+
+const command: ApplicationCommandData = {
+  name: "seek",
+  description: "seek.",
+  type: "CHAT_INPUT",
+  options: [
+    {
+      name: "time",
+      type: "STRING",
+      description: "Seek to time?",
+      required: true,
+    },
+  ],
+};
+
+registerCommand(command);
+
+addCommandHandler(command, async (interaction) => {
+  const member = interaction.member as GuildMember;
+  const guild = interaction.guild!;
+  const queue = getQueue(guild.id);
+
+  const connection = getVoiceConnection(guild.id);
+  if (!connection) {
+    join(member);
+  }
+  const [track, _] = queue.getTrack();
+ 
+  if (!track) {
+    await interaction.reply({ content: "No Track", ephemeral: true });
+    return;
+  }
+
+  const timeStr = interaction.options.getString("time");
+
+  if (!timeStr || parseFloat(timeStr) == NaN) return
+
+  const time = parseFloat(timeStr)
+
+  await queue.seekTo(time);
+
+  const seekTo = msToDuration(time * 1000);
+  const duration = msToDuration(track.duration);
+
+  interaction.replied || interaction.reply({ content: `✅ seeked to, ${seekTo} of ${duration}`, ephemeral: true });
+});
