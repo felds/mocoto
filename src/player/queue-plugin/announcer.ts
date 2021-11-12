bd4eca26d9e9f28b2372ba93466cc332295a3508
Felds Liscia <dev@felds.com.br>
2021-11-11 23:54:08 -0300

    show embed with pretty buttons

--- src/player/queue-plugin/announcer.ts
+++ src/player/queue-plugin/announcer.ts
@@ -1,18 +1,40 @@
+import { MessageEmbed } from "discord.js";
 import { getBot } from "../../bot";
 import { createBaseEmbed } from "../../util/message";
+import { msToDuration } from "../../util/string";
 import { QueuePlugin } from "../queue-plugin";
+import { Track } from "../track";
 
 const onPlay: QueuePlugin["onPlay"] = async (params) => {
   const bot = getBot(params.guildId);
   const channel = bot.textChannel;
   if (!channel) return;
 
-  const embed = createBaseEmbed();
-  embed.setDescription("Tá rolando música");
+  const embed = createEmbed(params.track);
 
   const message = await channel.send({
     embeds: [embed],
+    components: [
+      {
+        type: "ACTION_ROW",
+        components: [
+          {
+            customId: "StartOver",
+            type: "BUTTON",
+            style: "PRIMARY",
+            label: "Start over",
+          },
+          {
+            customId: "Skip",
+            type: "BUTTON",
+            style: "PRIMARY",
+            label: "Skip",
+          },
+        ],
+      },
+    ],
   });
+
   console.log("Message sent", message.id);
 };
 
@@ -20,3 +42,27 @@ const Announcer: QueuePlugin = {
   onPlay,
 };
 export default Announcer;
+
+function createEmbed(track: Track): MessageEmbed {
+  const embed = createBaseEmbed();
+
+  const author = track.userRef?.deref();
+  if (author) {
+    embed.setDescription(`${author} added a new track:\n**${track}**`);
+  } else {
+    embed.setDescription(`**${track}**`);
+  }
+
+  if (track.url) {
+    embed.addField("Url", track.url);
+    embed.setURL(track.url);
+  }
+
+  embed.addField("Duration", msToDuration(track.duration));
+
+  if (track.thumbnail) {
+    embed.setThumbnail(track.thumbnail);
+  }
+
+  return embed;
+}
