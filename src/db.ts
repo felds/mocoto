8605b6e3488469d11717c6b418ddf726cec3886b
Felds Liscia <dev@felds.com.br>
2021-11-15 00:08:49 -0300

    add getConfig function

--- src/db.ts
+++ src/db.ts
@@ -3,11 +3,20 @@ import { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore";
 import { FIREBASE_CREDENTIALS_PATH } from "./config";
 
 const serviceAccount = require(FIREBASE_CREDENTIALS_PATH);
-
-initializeApp({
-  credential: cert(serviceAccount),
-});
-
+initializeApp({ credential: cert(serviceAccount) });
 const db = getFirestore();
 
-export default db;
+export type BotConfig = {
+  textChannelId?: string;
+};
+
+export async function getConfig<T extends keyof BotConfig>(
+  guildId: string,
+  key: T,
+  fallback?: BotConfig[T],
+): Promise<BotConfig[T]> {
+  const doc = await db.collection("guilds").doc(guildId).get();
+  const data = doc.data();
+  const value = data?.[key];
+  return value ?? fallback;
+}
