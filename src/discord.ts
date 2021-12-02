21ec4c57436eded4d0d530acbd0866579ce23a7e
Felds Liscia <dev@felds.com.br>
2021-12-02 00:32:06 -0300

    mess up formatting

--- src/discord.ts
+++ src/discord.ts
@@ -1,11 +1,11 @@
 import { Client, Intents } from "discord.js";
 
 export const client = new Client({
-  intents: [
-    // ...
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILDS,
-  ],
-});
+          intents: [
+            // ...
+            Intents.FLAGS.GUILD_VOICE_STATES,
+            Intents.FLAGS.GUILDS,
+          ],
+        })
 
 client.setMaxListeners(0);
