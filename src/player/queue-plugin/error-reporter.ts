7121cbf92d3baa9b58b996ad88bacc23e464e3f1
Felds Liscia <dev@felds.com.br>
2021-12-03 01:17:46 -0300

    show sobbing gif on error

--- src/player/queue-plugin/error-reporter.ts
+++ src/player/queue-plugin/error-reporter.ts
@@ -1,8 +1,10 @@
 import { MessageEmbed } from "discord.js";
+import { noop, sample } from "lodash";
 import { client } from "../../discord";
 import { getPref, unsetPref } from "../../prefs";
 import { createBaseEmbed } from "../../util/message";
 import { QueuePlugin } from "../queue-plugin";
+import axios from "axios";
 
 const error: QueuePlugin["error"] = async ({ queue, error }) => {
   const { guildId } = queue;
@@ -17,24 +19,36 @@ const error: QueuePlugin["error"] = async ({ queue, error }) => {
     return;
   }
 
+  const embed = await createEmbed(error);
+
   await channel.send({
-    embeds: [createEmbed(error)],
+    embeds: [embed],
   });
 };
 
-const ErrorReporter: QueuePlugin = {
-  error,
-};
-export default ErrorReporter;
-
-/* ====================================================== */
-function createEmbed(error: Error): MessageEmbed {
+async function createEmbed(error: Error): Promise<MessageEmbed> {
   const embed = createBaseEmbed();
 
   embed.setDescription(
     "There was a ploblem playing the current track. Skipping...",
   );
-  embed.addField("Message", `\`${error.message}\``);
+
+  await randomGif()
+    .then((gif) => embed.setImage(gif))
+    .catch(noop);
 
   return embed;
 }
+
+async function randomGif(): Promise<string> {
+  const count = 100;
+  const { data } = await axios.get("https://api.gfycat.com/v1/gfycats/search", {
+    params: { search_text: "sobbing", count },
+  });
+  return sample(data.gfycats).gifUrl;
+}
+
+const ErrorReporter: QueuePlugin = {
+  error,
+};
+export default ErrorReporter;
