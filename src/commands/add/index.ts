fb0cfa8f46afb28d197fc40ed761e3cd86ecb726
Felds Liscia <dev@felds.com.br>
2021-12-09 00:04:08 -0300

    add dummy menu

--- src/commands/add/index.ts
+++ src/commands/add/index.ts
@@ -1,4 +1,11 @@
-import { ApplicationCommandData, Message } from "discord.js";
+import {
+  ApplicationCommandData,
+  Emoji,
+  Message,
+  MessageActionRow,
+  MessageOptions,
+  ReactionEmoji,
+} from "discord.js";
 import { addCommandHandler, registerCommand } from "../../util/discord";
 import { setTimeout } from "timers/promises";
 import assert from "assert/strict";
@@ -21,8 +28,22 @@ const command: ApplicationCommandData = {
 registerCommand(command);
 
 addCommandHandler(command, async (interaction) => {
-  await interaction.deferReply();
+  await interaction.deferReply({ ephemeral: true });
 
-  await setTimeout(100);
-  interaction.editReply("Editando o reply");
+  // prettier-ignore
+  const components: MessageOptions['components'] = 
+  [
+    {type: "ACTION_ROW", components: [
+      { type: "SELECT_MENU", customId: 'track', minValues: 0, maxValues: 2, options: [
+        { value: '001', label: "O Rappa - My Brother", emoji: '♾' },
+        { value: '002', label: "Salvatore Ganacci - Heartbass (feat. Tommy Cash) [Official Audio]", emoji: '▶️' },
+      ] },
+    ]},
+    {type: "ACTION_ROW", components: [
+      { type: "BUTTON", customId: 'prev', style: "PRIMARY", label: "Prev", disabled: true },
+      { type: "BUTTON", customId: 'next', style: "PRIMARY", label: "Next" },
+    ]},
+  ];
+
+  interaction.editReply({ content: "/** TODO: add embed */", components });
 });
