1468dbf0f11da2088386df2eb6b51ba3088a5adc
Felds Liscia <dev@felds.com.br>
2021-12-11 02:15:09 -0300

    convert list command

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,56 +1,49 @@
-import { ApplicationCommandData, GuildMember } from "discord.js";
+import { embedComponent, Gatekeeper } from "@itsmapleleaf/gatekeeper";
+import assert from "assert/strict";
 import { getQueue } from "../player/queue";
-import { Track } from "../player/track";
-import { addCommandHandler, registerCommand } from "../util/discord";
 import { createBaseEmbed } from "../util/message";
 import { msToDuration } from "../util/string";
 
-const command: ApplicationCommandData = {
-  type: "CHAT_INPUT",
-  name: "list",
-  description: "List tracks.",
-};
+/** @todo Add prev/next page buttons. */
+export default function listCommand(gatekeeper: Gatekeeper) {
+  gatekeeper.addSlashCommand({
+    name: "list",
+    description: "List tracks.",
+    async run(context) {
+      const guild = context.guild;
+      assert(guild);
 
-registerCommand(command);
+      const queue = getQueue(guild.id);
+      const current = queue.getCurrent();
+      const list = queue.getTracks();
 
-addCommandHandler(command, async (interaction) => {
-  const guild = interaction.guild!;
-  const queue = getQueue(guild.id);
+      if (!list.length) {
+        const embed = createBaseEmbed().setDescription("The queue is empty");
+        return context.ephemeralReply(() => [embedComponent(embed)]);
+      }
 
-  const current = queue.getCurrent();
-  const list = queue.getTracks(); //.slice(Math.max(0, current - 1));
+      const lines = list
+        .map((track, i) => {
+          const duration = msToDuration(track.duration);
+          if (i - current < -2) return null; /** @xx wtf? */
 
-  if (!list.length) {
-    await interaction.reply({ content: "No Tracks", ephemeral: true });
-    return;
-  }
+          const index = String(i - current).padStart(2, "0");
+          const user = track.userRef?.deref();
+          let usertext = "";
+          if (user) usertext = `from (${user})`;
+          const trackTitle =
+            String(track).length > 30
+              ? String(track).substring(0, 30) + "..."
+              : track;
+          return `**${index}** \`${trackTitle} -- (${duration})\`${usertext}`;
+        })
+        .filter((x) => x != null);
 
-  const lines = list
-    .map((track, i) => {
-      const duration = msToDuration(track.duration);
+      const embed = createBaseEmbed()
+        .setTitle(`Tracks (${current} - ${list.length})`)
+        .setDescription(lines.join("\n"));
 
-      if (i - current < -2) return null;
-
-      const index = String(i - current).padStart(2, "0");
-
-      const user = track.userRef?.deref();
-      let usertext = "";
-      if (user) usertext = `from (${user})`;
-      const trackTitle =
-        String(track).length > 30
-          ? String(track).substring(0, 30) + "..."
-          : track;
-
-      return `**${index}** \`${trackTitle} -- (${duration})\`${usertext}`;
-    })
-    .filter((x) => x != null);
-
-  const embed = createBaseEmbed();
-  embed.setTitle(`Tracks (${current} - ${list.length})`);
-  embed.setDescription(lines.join("\n"));
-
-  await interaction.reply({
-    embeds: [embed],
-    ephemeral: true,
+      context.ephemeralReply(() => [embedComponent(embed)]);
+    },
   });
-});
+}
